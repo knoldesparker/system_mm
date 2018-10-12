4ee65f0c4e90c2f6573c5eddf68c5e85d127a5c2
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-10-12 09:45:48 +0200

    made a login

---
+++ src/main/java/com/example/demo/Controllers/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String homePage()
+    {
+        return "index";
+    }
+
+    @RequestMapping("/index")
+    public String backToHome()
+    {
+        return "index";
+    }
+
+    @RequestMapping("/test")
+    public String testPage()
+    {
+        return "test";
+    }
+}
